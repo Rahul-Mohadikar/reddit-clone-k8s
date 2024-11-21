0a90652f90ad7a1a6f3e85add69c2d2e4952bba6
Rahul-Mohadikar <152637813+Rahul-Mohadikar@users.noreply.github.com>
2024-11-21 09:19:11 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "rahul07070" # Replace with your actual S3 bucket name
+    bucket = "reddit-rahul" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
